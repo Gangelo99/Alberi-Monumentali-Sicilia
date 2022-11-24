90d05f73d36417e7ff3f0fff9eaea423b20a1cc5
Angelo Franciamore <angelo99franciamore@gmail.com>
2022-11-24 09:29:41 +0100

    fix: some fixes

---
+++ module/input_database.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+input_file = "database/alberi-monumentali-sicilia.csv"
+data = pd.read_csv(input_file)
--- module/sum_trees_province.py
+++ module/sum_trees_province.py
@@ -1,11 +1,9 @@
 #This file calculate the total number of Tree in a province
 
-###Import Packages
-import pandas as pd
+### Import Packages
+from module.input_database import data, pd
 
-###Input the .csv file
-input_file = "alberi-monumentali-sicilia.csv"
-data = pd.read_csv(input_file)
+### Input the .csv file
 df = pd.DataFrame(data, columns=['PROVINCIA', 'COMUNE', 'PROGR'])
 
 ### Groupby, count and sortvalues
@@ -15,7 +13,7 @@ group_provincia = group_provincia.count().sort_values(ascending= False) #Countin
 
 ### Create the lists of the province and obtain the list of the total number of trees 
 global provincia_list
-provincia_list = group_provincia.index.to_list() # Obtain a list of the names of various province
+provincia_list = group_provincia.index.to_list() #Obtain a list of the names of various province
 
 global total_number_list
 total_number_list = group_provincia.values.tolist() #Obtain the list of the sum of the trees present in each province
