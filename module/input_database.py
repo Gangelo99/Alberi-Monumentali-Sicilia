3b81bf8a1eaddc43280498932c8010e09e8ca104
Angelo Franciamore <angelo99franciamore@gmail.com>
2022-11-25 18:05:52 +0100

    feat!: converted ALTEZZA column in int type
    This conversion is necessary for calculate the max height of the trees

--- module/input_database.py
+++ module/input_database.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-input_file = "database/alberi-monumentali-sicilia.csv"
+input_file = "../database/alberi-monumentali-sicilia.csv"
 data = pd.read_csv(input_file)
+
+data['ALTEZZA (m)'] = data['ALTEZZA (m)'].astype(int) #Convert the ALTEZZA column from string to int
+
+# check_type = data.dtypes
+# print(check_type)
