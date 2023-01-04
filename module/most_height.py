2fac21d9b99c47b5ab7cd78b4dd424aa58711a2e
Angelo Franciamore <angelo99franciamore@gmail.com>
2023-01-04 11:17:54 +0100

    add: added two variables list

--- module/most_height.py
+++ module/most_height.py
@@ -4,12 +4,19 @@ from input_database import pd, data
 
 df = pd.DataFrame(data, columns = ['COMUNE', 'ALTEZZA (m)','SPECIE NOME VOLGARE'])
 
-# most_height = df.groupby('COMUNE')
-# most_height = most_height['ALTEZZA (m)']
-# most_height = most_height.max()
+most_height = df.groupby('COMUNE').max().sort_values(by= ['ALTEZZA (m)'],ascending= False)
+
+provincia_list = most_height.index.to_list() #Obtain a list of the names of various province
+high_list = most_height['ALTEZZA (m)'].values.tolist()
+name_trees_list = most_height['SPECIE NOME VOLGARE'].values.tolist()
 
 # print(most_height)
 
-most_height = df.groupby('COMUNE').max().sort_values(by= ['ALTEZZA (m)'],ascending= False)
+# print("LISTA ROBE:\n")
+# print(provincia_list)
 
-print(most_height)
+# print("LISTA ROBE:\n")
+# print(high_list)
+
+# print("LISTA ROBE:\n")
+# print(name_trees_list)
